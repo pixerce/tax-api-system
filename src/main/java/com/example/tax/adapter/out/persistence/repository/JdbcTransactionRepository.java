64044807db9f069ae955737e633c6b5f475dfb87
jongseok <pixerce@gmail.com>
2026-01-06 13:59:12 +0900

    refactor: jdbc @Transactional 추가
    - batch insert의 caller와 callee가 모두 100개씩 데이터를 전송하고 있지만, 추후 caller에서 더 많은 데이터를 요청하고
      트랜잭션으로 묶어야 하는 상황이 발생할 수 있으므로 @Transactional 추가함.

--- src/main/java/com/example/tax/adapter/out/persistence/repository/JdbcTransactionRepository.java
+++ src/main/java/com/example/tax/adapter/out/persistence/repository/JdbcTransactionRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.PreparedStatement;
 import java.util.List;
@@ -14,15 +15,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class JdbcTransactionRepository {
 
+    private final static Integer DEFAULT_BATCH_SIZE = 100;
     private final JdbcTemplate jdbcTemplate;
 
+    @Transactional
     public void saveAll(List<TransactionRecord> transactions) {
         log.info("Saving transactions: {}", transactions);
         String sql = "INSERT INTO tax.transaction_record (transaction_type, amount, store_id, transaction_date, created_at) VALUES (?, ?, ?, ?, now())";
 
         jdbcTemplate.batchUpdate(sql,
                 transactions,
-                100,
+                DEFAULT_BATCH_SIZE,
                 (PreparedStatement ps, TransactionRecord transaction) -> {
                     ps.setString(1, transaction.getTransactionType().name());
                     ps.setBigDecimal(2, transaction.getAmount());
